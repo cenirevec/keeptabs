7626cd22a4fe411b41dfe50ebc81a3e58a6588ad
Corentin ENIREVEC <74110184+cenirevec@users.noreply.github.com>
2022-08-28 20:08:38 +0200

    Removing Build directory

--- dist/background.js
+++
@@ -1,9 +0,0 @@
-if(chrome != undefined && chrome.action != undefined){
-    chrome.action.onClicked.addListener(() => {
-        chrome.tabs.create({"url": "/home.html"});
-    });
-}else{
-    browser.browserAction.onClicked.addListener(() => {
-        browser.tabs.create({"url": "/home.html"});
-    });
-}
