c11a53463dc1ec729f8d2fd8228fc188bcbe527f
Corentin ENIREVEC <c.enirevec@gmail.com>
2026-07-22 02:34:01 +0200

    Switching to manifest V3

--- dist/api/services/data/category.service.mjs
+++ dist/api/services/data/category.service.mjs
@@ -1,1 +1,114 @@
-import{Services as e}from"../../../../src/services.jsx";export class CategoryService{DAYS=864e5;defaultTabGroupData={meta:{lastAccessed:(new Date).valueOf(),name:null},tabs:[]};defaultCategoryData={meta:{name:"newCategory",expiration:-1,hidden:-1},tabGroups:[]};constructor(){this.create=this.create.bind(this)}get(a){return e.data?.model?.categories[a]}getByName(a){return Object.keys(e.data.model.categories).map(a=>e.data.model.categories[a]).find(e=>e.meta.name==a)}generateCategoryId(){let a=Object.keys(e.data?.model?.categories);return parseInt(a[a.length-1])+1}rename(a,t){a.meta.name=t,e.data.save(),e.emitSignal("categoryListChange")}create(a,t=!0){let r=this.generateCategoryId(),s=JSON.parse(JSON.stringify(this.defaultCategoryData));return s.meta.name=a,e.data.model.categories[r]=s,t&&e.data.save(),s}delete(a){delete e.data?.model?.categories[a],e.data.save()}configure(a,t,r){"expiration"==t&&NaN!=parseInt(r)&&(a.meta.expiration=parseInt(r)),"hidden"==t&&NaN!=parseInt(r)&&(a.meta.hidden=parseInt(r)),e.data.save()}getSetting(e,a){return e.meta[a]}}export default CategoryService;
+import { Services } from "../../../../src/services.jsx";
+
+export class CategoryService {
+
+    DAYS = 24*3600*1000;
+
+    defaultTabGroupData = {
+        "meta": {
+            "lastAccessed": new Date().valueOf(),
+            "name":null
+        },
+        "tabs": []
+    }
+
+    defaultCategoryData = {
+        "meta":{
+            name: "newCategory",
+            expiration: -1, // 30*this.DAYS,
+            hidden: -1//14*this.DAYS
+        },
+        "tabGroups":[]
+    }
+
+    constructor(){
+        this.create = this.create.bind(this);
+    }
+
+    /**
+     * Get a category by its index
+     * @param {*} categoryId Index of the category
+     * @returns 
+     */
+    get(categoryId){
+        return Services.data?.model?.categories[categoryId];
+    }
+
+    /**
+     * Get a category by its name
+     * @param {*} name Name of the category
+     * @returns 
+     */
+    getByName(name){
+        let categories = Object.keys(Services.data.model.categories)
+                               .map(categoryid=>Services.data.model.categories[categoryid])
+        return categories.find(category => category.meta.name == name);
+    }
+
+    /**
+     * Generate an unique category id
+     * @returns A new category id
+     */
+    generateCategoryId(){
+        let ids = Object.keys(Services.data?.model?.categories);
+        return parseInt(ids[ids.length -1]) + 1;
+    }
+
+    /**
+     * Rename a category
+     * @param {object} category The category to rename
+     * @param {string} newName Name to give to the category
+     * @returns True if the operation succeeded, False otherwise
+     */
+    rename(category,newName){
+        category.meta.name = newName;
+        Services.data.save();
+        Services.emitSignal("categoryListChange");
+    }
+
+    /**
+     * Create a category
+     * @param {string} name Name of the category
+     */
+    create(name,save = true){
+        //Get an unique id
+        let id = this.generateCategoryId();
+
+        //Create a new category object
+        let category = JSON.parse(JSON.stringify(this.defaultCategoryData));
+        category.meta.name = name;
+
+        //Add the category to the object
+        Services.data.model.categories[id] = category;
+       
+        if(save) Services.data.save();
+       // Services.emitSignal("categoryListChange");
+        return category;
+    }
+
+    /**
+     * Delete a category
+     * @param {*} id 
+     */
+    delete(id){
+        delete Services.data?.model?.categories[id];
+        Services.data.save();
+    }
+
+    configure(category,setting,value){
+        if(setting == "expiration" && (parseInt(value) != NaN)){
+            category.meta.expiration = parseInt(value)
+        }
+
+        if(setting == "hidden" && parseInt(value) != NaN){
+            category.meta.hidden = parseInt(value)
+        }
+        Services.data.save();
+    }
+
+    getSetting(category,setting){
+        return category.meta[setting]
+    }
+}
+
+export default CategoryService;
--- public/engine.js
+++ public/engine.js
@@ -1,3 +1,16 @@
+//Opens a keeptabs instance when the keeptabs button is clicked
+if(chrome != undefined && chrome.action != undefined){
+    chrome.action.onClicked.addListener(() => {
+        chrome.tabs.create({"url": "/home.html"});
+    });
+}else{
+    browser.browserAction.onClicked.addListener(() => {
+        browser.tabs.create({"url": "/home.html"});
+    });
+}
+
+
+
 class InstanceController {
 
     static data = {
